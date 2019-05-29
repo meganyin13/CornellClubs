69d07337e7c6523004c9d1fe5d372ae38ff862cf
meganyin13 <my474@cornell.edu>
2019-05-29 13:35:12 -0400

    finished first screen

---
+++ cornellclubs/frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Homepage from "./components/Home/Homepage";
+
+function App() {
+  return (
+    <Homepage/>
+  );
+}
+
+export default App;
---
+++ cornellclubs/frontend/src/components/Home/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = (props) => {
+    return (
+        <div className="title">
+            <div className="title-text">
+                <h1>Cornell: Any Person, Any Club</h1>
+                <h3>Find your dream club today.</h3>
+            </div>
+        </div>
+    )
+};
+
+export default Homepage;
